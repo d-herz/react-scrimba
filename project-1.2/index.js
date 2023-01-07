49fd7b9de5521f6e64b8b5c21c9d557b23c6e95b
Daniel Hyres <danhyres@gmail.com>
2023-01-07 10:43:11 -0500

    first commit

---
+++ project-1.2/index.js
@@ -0,0 +1,18 @@
+// import React from "react"
+// import ReactDOM from "react-dom"
+
+const page = (
+    <div>
+        <img src="./react-logo.png" width="40px" />
+        <h1>FUN FACTS ABOUT REACT</h1>
+        <ul>
+            <li>First released 2013</li>
+            <li>Created by Jordan Walke</li>
+            <li>Maintained by Meta</li>
+            <li>Powers many apps</li>
+
+        </ul>
+    </div>
+)
+
+ReactDOM.render(page, document.getElementById("root")) 
