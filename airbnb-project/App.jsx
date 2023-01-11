328fa78f22595e07d474405cce37129141a616f8
Daniel Hyres <danhyres@gmail.com>
2023-01-11 11:44:26 -0500

    Added Props

--- airbnb-project/App.jsx
+++ airbnb-project/App.jsx
@@ -2,18 +2,23 @@ import React from "react"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Card from "./components/Card"
+import Swimmer from "./image/katie-zaferes.png"
 
-/*
-Challenge: Build the Navbar component.
-Check the Figma file for the design specifics.
-*/
+
 
 export default function App() {
   return (
     <div className="container">
       <Navbar />
       <Hero />
-      <Card />
+      <Card
+        title="Life Lessons with Katie Zaferes"
+        img={Swimmer}
+        rating="5.0"
+        reviewCount={6}
+        country="USA"
+        price={136}
+      />
       
     </div>
   )
