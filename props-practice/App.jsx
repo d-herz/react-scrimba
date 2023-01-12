ff459693aff1d2562d326dcb129d7104d03cdff6
Daniel Hyres <danhyres@gmail.com>
2023-01-12 11:49:55 -0500

    Props Practice

--- props-practice/App.jsx
+++ props-practice/App.jsx
@@ -1,35 +1,45 @@
 import React from "react"
 import Joke from "./components/Joke"
+import jokesData from "./jokesData"
 
 
 function App() {
-
+  // console.log(jokesData)
+  const jokeElements = jokesData.map(joke => {
+    return <Joke
+      setup={joke.setup}
+      punchline={joke.punchline}
+    />
+  })
+  
   return (
     <div className='container'>
-      <Joke
-        punchline="It's hard to explain puns to kleptomaniacs because they always take things literally."
-      />
-      <Joke
-        setup="I got my daughter a fridge for her birthday."
-        punchline="I can't wait to see her face light up when she opens it."
-      />
-      <Joke
-        setup="How did the hacker escape the police?"
-        punchline="He just ransomware!"
-      />
-      <Joke
-        setup="Why don't pirates travel on mountain roads?"
-        punchline="Scurvy."
-      />
-      <Joke
-        setup="Why do bees stay in the hive in the winter?"
-        punchline="Swarm."
-      />
-      <Joke
-        setup="What's the best thing about Switzerland?"
-        punchline="I don't know, but the flag is a big plus!"
-      />  
+      {jokeElements}
     </div>
   )
 }
 export default App
+
+/* <Joke
+  punchline="It's hard to explain puns to kleptomaniacs because they always take things literally."
+/>
+<Joke
+  setup="I got my daughter a fridge for her birthday."
+  punchline="I can't wait to see her face light up when she opens it."
+/>
+<Joke
+  setup="How did the hacker escape the police?"
+  punchline="He just ransomware!"
+/>
+<Joke
+  setup="Why don't pirates travel on mountain roads?"
+  punchline="Scurvy."
+/>
+<Joke
+  setup="Why do bees stay in the hive in the winter?"
+  punchline="Swarm."
+/>
+<Joke
+  setup="What's the best thing about Switzerland?"
+  punchline="I don't know, but the flag is a big plus!"
+/>   */
--- props-practice/components/Joke.jsx
+++ props-practice/components/Joke.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
 export default function Joke(props) {
-  console.log(props)
+  // console.log(props)
   return (
     <div className="joke-container">
-      <h3>{props.setup}</h3>
-      <p>{props.punchline}</p>
+      {props.setup && <h3>Setup: {props.setup}</h3>}
+      <p>Punchline: {props.punchline}</p>
       <hr />
     </div>
     
