ea71300b96b66cdeb9587be2800ca1168d525321
Daniel Hyres <danhyres@gmail.com>
2023-01-12 12:59:31 -0500

    AirBnB Updates

--- airbnb-project/App.jsx
+++ airbnb-project/App.jsx
@@ -2,24 +2,32 @@ import React from "react"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Card from "./components/Card"
-import Swimmer from "./image/katie-zaferes.png"
-
+import data from "./data"
 
 
 export default function App() {
+  const cardElement = data.map(data => {
+    return <Card
+      key={data.id}
+      title={data.title}
+      description={data.description}
+      price={data.price}
+      img={`./image/${data.coverImg}`}
+      location={data.location}
+      rating={data.stats.rating}
+      reviewCount={data.stats.reviewCount}
+      openSpots={data.openSpots}
+    />
+  })
+
   return (
     <div className="container">
       <Navbar />
       <Hero />
-      <Card
-        title="Life Lessons with Katie Zaferes"
-        img={Swimmer}
-        rating="5.0"
-        reviewCount={6}
-        country="USA"
-        price={136}
-      />
-      
+      <section className="cards--list">
+        {cardElement}
+      </section>
     </div>
   )
 }
+
--- airbnb-project/components/Card.jsx
+++ airbnb-project/components/Card.jsx
@@ -2,22 +2,28 @@ import React from "react";
 
 import Star from "../image/star.png"
 
-import Swimmer from "../image/katie-zaferes.png"
 
 
 export default function Card(props) {
+  let badgeText
+  if (props.openSpots === 0) {
+    badgeText = "SOLD OUT"
+  } else if (props.location === "Online") {
+    badgeText = "ONLINE"
+  }
 
   return (
     <div className="card">
+      {badgeText && <div className="card--badge">{badgeText}</div>}
       <img src={props.img} className="card--image" />
       <div className="card--stats">
         <img src={Star} className="card--star" />
         <span>{props.rating}</span>
         <span className="gray">({props.reviewCount}) • </span>
-        <span className="gray"> {props.country}</span>
+        <span className="gray"> {props.location}</span>
       </div>
-      <p>{props.title}</p>
-      <p><span className="bold"> From ${props.price} </span>/ person </p>
+      <p className="card--title">{props.title}</p>
+      <p className="card--price"><span className="bold"> From ${props.price} </span>/ person </p>
     </div>
   )
 
