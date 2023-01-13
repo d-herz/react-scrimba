797043e4182dbf7f11feb96f6e723474f299a4bc
Daniel Hyres <danhyres@gmail.com>
2023-01-13 12:03:18 -0500

    travel project first commit

---
+++ travel-joiurnal-project/components/Card.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default function Card(props) {
+  
+  return (
+    <div className="card--main">
+
+      <div className="card--img">
+        <img src={props.imageUrl}></img>
+      </div>
+
+      <div className="card--info">
+        <div className="card--title">
+          <span><i className="fa-solid fa-location-dot"></i> {props.location}</span>
+          <a href={props.googleMapsUrl}>View on Google Maps</a>
+          <h1>{props.title}</h1>
+        </div>
+
+        <div className="card--details">
+          <h3>{props.startDate} - {props.endDate}</h3>
+          <p>{props.description}</p>
+        </div>
+
+      </div>
+
+    </div>
+  )
+}
