50b5d177c7c34a1c9d57c9feb74cdddc5dc986bf
Daniel Hyres <danhyres@gmail.com>
2023-01-20 11:05:32 -0500

    added side effect for fetching from memes API

--- react-meme-generator/components/MemeForm.jsx
+++ react-meme-generator/components/MemeForm.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import memesData from "../memesData.js"
+// import memesData from "../memesData.js"
 
 export default function MemeForm() {
 
@@ -9,27 +9,38 @@ export default function MemeForm() {
     randomImage: "http://i.imgflip.com/1bij.jpg",
   })
   
-  // const [allMemeImages, setAllMemeImages] = React.useState(memesData) //what is this for?
+  const [allMemes, setAllMemes] = React.useState([]) 
 
+  React.useEffect(() => {
+    fetch(`https://api.imgflip.com/get_memes`)
+      .then(res => res.json())
+      .then(data => {
+        // console.log(data.data.memes)
+        return setAllMemes(data.data.memes)
+      })
+    
+  },[])
+
+  
+  function fetchMeme (){
+    const memeArr = memesData.data.memes
+    const ranNum = Math.floor(Math.random() * memeArr.length);
+    const url = memeArr[ranNum].url
+    
+    setMeme(prevMeme => {
+      return {
+        ...prevMeme,
+        randomImage: url
+      }
+    })
+  }
+  
   function handleChange(event) {
     const {name, value} = event.target
     setMeme(prevMeme => {
       return {
         ...prevMeme,
         [name]: value
-      }
-    })
-  }
-
-  function fetchMeme () {
-    const memeArr = memesData.data.memes
-    const ranNum = Math.floor(Math.random() * memeArr.length);
-    const url = memeArr[ranNum].url
-
-    setMeme(prevMeme => {
-      return {
-        ...prevMeme,
-        randomImage: url
       }
     })
   }
